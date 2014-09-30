dda74f2d3e597afa92ed99bec95b6a426bb17d5e
Federico De Faveri <federico.defaveri@fao.org>
2014-09-30 21:54:11 +0200

    Rounded centered with different sizes boxes with upper case names

--- lib/home/page_tile.dart
+++ lib/home/page_tile.dart
@@ -6,10 +6,15 @@ class PageTile extends PolymerElement {
   @published String name;
   @published String theme;
   
+  @published num hratio;
+  @published num wratio;
+  
   PageTile.created() : super.created();
 
   void tileSelected() {
     this.fire("tile-selected", detail:name);
   }
+  
+  uppercase(String str) => str.toUpperCase();
 
 }
