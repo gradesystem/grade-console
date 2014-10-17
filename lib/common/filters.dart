35e3a4283ef00de6e71b475f6c7c83ccc6b2a086
Federico De Faveri <federico.defaveri@fao.org>
2014-10-17 11:58:44 +0200

    Added fields sorting

--- lib/common/filters.dart
+++ lib/common/filters.dart
@@ -23,4 +23,19 @@ class Filters {
   }
   
   filter(String term, FilterFunction filterFunction) => (List items) => term.isEmpty ? items : toObservable(items.where((item)=>filterFunction(item, term)).toList());
+  
+  partialOrdering(List<String> partialOrdering) {
+    return (Map toOrder) {
+      List<String> ordered = new List<String>();
+      
+      for (String orderedValue in partialOrdering) {
+        if (toOrder.containsKey(orderedValue)) ordered.add(orderedValue);
+      }
+      
+      for (String toOrderValue in toOrder.keys) {
+         if (!ordered.contains(toOrderValue)) ordered.add(toOrderValue);
+      }
+      return ordered;
+    };
+  }
 }
--- lib/tasks/task_details.dart
+++ lib/tasks/task_details.dart
@@ -3,9 +3,24 @@ part of tasks;
 @CustomTag("task-details") 
 class TaskDetails extends PolymerElement with Filters {
   
+  static List<String> ORDERED_FIELDS = [
+                                        "http://www.w3.org/2000/01/rdf-schema#label",
+                                        "http://semanticrepository/publisher/task#source_graph",
+                                        "http://semanticrepository/publisher/task#target_graph",
+                                        "http://semanticrepository/publisher/task#query",
+                                        "http://semanticrepository/publisher/task#operation",
+                                        "http://purl.org/dc/terms/creator",
+                                        "http://www.w3.org/2004/02/skos/core#editorialNote"
+                                        
+                                        ];
+  
+  
+  
   @published
   Task item;
   
   TaskDetails.created() : super.created();
+  
+  List<String> get orderedFields => ORDERED_FIELDS;
   
 }
