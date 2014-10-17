73346ea737c77b8639e4be1e66c25a7649fde7db
Federico De Faveri <federico.defaveri@fao.org>
2014-10-17 17:44:58 +0200

    Task details now visualized as form

--- lib/tasks/task_details.dart
+++ lib/tasks/task_details.dart
@@ -4,17 +4,28 @@ part of tasks;
 class TaskDetails extends PolymerElement with Filters {
   
   static List<String> ORDERED_FIELDS = [
+                                        "http://gradesystem.io/onto#id",
                                         "http://www.w3.org/2000/01/rdf-schema#label",
-                                        "http://semanticrepository/publisher/task#source_graph",
-                                        "http://semanticrepository/publisher/task#target_graph",
-                                        "http://semanticrepository/publisher/task#query",
-                                        "http://semanticrepository/publisher/task#operation",
+                                        "http://gradesystem.io/onto/task.owl#source_graph",
+                                        "http://gradesystem.io/onto/task.owl#target_graph",
+                                        "http://gradesystem.io/onto/task.owl#query",
+                                        "http://gradesystem.io/onto/task.owl#operation",
                                         "http://purl.org/dc/terms/creator",
                                         "http://www.w3.org/2004/02/skos/core#editorialNote"
                                         
                                         ];
   
+  static List<String> AREA_FIELDS = [
+                                          "http://gradesystem.io/onto/task.owl#query",
+                                                 "http://www.w3.org/2004/02/skos/core#editorialNote"
+                                          
+                                          ];
+    
+  @published
+  bool editable = false;
   
+  @published
+  String test;
   
   @published
   Task item;
@@ -22,5 +33,9 @@ class TaskDetails extends PolymerElement with Filters {
   TaskDetails.created() : super.created();
   
   List<String> get orderedFields => ORDERED_FIELDS;
+  
+  bool isAreaField(String key) {
+    return AREA_FIELDS.contains(key);
+  }
   
 }
