ad1ae56e2918b4d5e24e86f166a577133c3c6d10
Federico De Faveri <federico.defaveri@fao.org>
2014-10-20 12:30:57 +0200

    Added subpages

---
+++ lib/common/components/grade_subpages.dart
@@ -0,0 +1,18 @@
+import 'package:polymer/polymer.dart';
+
+@CustomTag('grade-subpages')
+class GradeSubpages extends PolymerElement {
+  
+  @observable
+  int subpage = 0;
+  
+  GradeSubpages.created() : super.created();
+  
+  void onMenuItemSelected(event, detail, target) {
+    switch (detail) {
+      case 'datasets': subpage = 0; break;
+      case 'queries': subpage = 1; break;
+    }
+  }
+  
+}
