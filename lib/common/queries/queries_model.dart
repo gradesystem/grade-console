d4d733b4c030c2d34f1938f84d09226c4c89b13e
Federico De Faveri <federico.defaveri@fao.org>
2014-10-22 17:34:47 +0200

    Added delete for queries

--- lib/common/queries/queries_model.dart
+++ lib/common/queries/queries_model.dart
@@ -7,18 +7,18 @@ class Query extends Delegate with ListItem {
   static String note_field= "http://gradesystem.io/onto/query.owl#note";
   static String target_field="http://gradesystem.io/onto/query.owl#target";
   static String expression_field="http://gradesystem.io/onto/query.owl#expression";
+  static String predefined_field="http://gradesystem.io/onto/query.owl#predefined";
    
     
   Query(this.repo_path, Map bean) : super(bean);
    
   final String repo_path;
+
+  String get name => this.get(name_field);
     
   String get endpoint => '../service/${repo_path}/query/${bean[name_field]}/results';
-      
-  String get title => this.get(name_field);
   
-  String get subTitle =>  this.endpoint;
-   
+  bool get predefined => get(predefined_field);
 }
 
 abstract class Queries extends ListItems<Query> {
--- lib/common/queries/query_list.dart
+++ lib/common/queries/query_list.dart
@@ -11,7 +11,7 @@ class QueryList extends PolymerElement with Filters {
   
   CoreList list;
   
-  FilterFunction itemFilter = (ListItem item, String term) => item.title != null && item.title.toLowerCase().contains(term.toLowerCase());
+  FilterFunction itemFilter = (Query item, String term) => item.name != null && item.name.toLowerCase().contains(term.toLowerCase());
   
   QueryList.created() : super.created();
   
@@ -31,6 +31,10 @@ class QueryList extends PolymerElement with Filters {
   
   void selectDataset(event) {
     listitems.selected = event.detail.data;
+  }
+  
+  void removeItem() {
+    listitems.data.remove(listitems.selected);
   }
  
 }
