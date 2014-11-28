83f8f65d911441d89a8f32060ce5bee1fdaac63e
Federico De Faveri <federico.defaveri@fao.org>
2014-11-28 10:17:42 +0100

    Fixed results title calculation

--- lib/deck/deck_page.dart
+++ lib/deck/deck_page.dart
@@ -12,12 +12,12 @@ class DeckPage extends Polybase {
   EditableTask asEditableTask(Task task) => task!=null?new EditableTask(task):null;
   Task getTask(RunningTask running) => running!=null?running.task:null;
   
-  @ComputedProperty('deckModel.storage.selected.execution.task.label')
+  @ComputedProperty('deckModel.storage.selected.task.label')
   String get title {
     
     try {
       
-      return "Results for '${deckModel.storage.selected.execution.task.label}'";
+      return "Results for '${deckModel.storage.selected.task.label}'";
     }
     on NoSuchMethodError {
       return "(no run selected)";
