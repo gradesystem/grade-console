ed35443d2c55e3c8dc20d7d164b7bf5628d1cdf1
Federico De Faveri <federico.defaveri@fao.org>
2014-12-16 10:05:24 +0100

    Housekeeping

--- lib/tasks/task_list.dart
+++ lib/tasks/task_list.dart
@@ -1,44 +1,40 @@
 part of tasks;
 
-@CustomTag("task-list") 
+@CustomTag("task-list")
 class TaskList extends PolymerElement with Filters {
-  
+
   @published
   String kfilter = '';
-  
+
   @published
   ListItems listitems;
-  
+
   CoreList list;
-  
+
   bool clickflag = false;
-  
-  FilterFunction itemFilter = (EditableModel<Task> item, String term) 
-                                => item.model.label != null && 
-                                   item.model.label.toLowerCase().contains(term.toLowerCase());
-  
+
+  FilterFunction itemFilter = (EditableModel<Task> item, String term) => item.model.label != null && item.model.label.toLowerCase().contains(term.toLowerCase());
+
   TaskList.created() : super.created();
-  
+
   void ready() {
     list = $['list'] as CoreList;
 
     onPropertyChange(listitems, #selected, syncSelection);
   }
-   
-   void syncSelection() {
-     if (listitems.selected!= null && listitems.selected != list.selection) {
-       if (listitems.selected == null) list.clearSelection();
-       else {
-         int index = listitems.data.indexOf(listitems.selected);
-         list.selectItem(index);
-       }
-     }
-   }  
-  
+
+  void syncSelection() {
+    if (listitems.selected != null && listitems.selected != list.selection) {
+      if (listitems.selected == null) list.clearSelection(); else {
+        int index = listitems.data.indexOf(listitems.selected);
+        list.selectItem(index);
+      }
+    }
+  }
+
   void selectItem(event) {
     //workaround to https://github.com/dart-lang/core-elements/issues/160
-    if (!clickflag) listitems.selected = event.detail.data;
-    else {
+    if (!clickflag) listitems.selected = event.detail.data; else {
       syncSelection();
       clickflag = false;
     }
@@ -47,5 +43,5 @@ class TaskList extends PolymerElement with Filters {
   void setClickFlag() {
     clickflag = true;
   }
- 
+
 }
