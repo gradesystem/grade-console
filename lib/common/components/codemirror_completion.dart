67e33d2d57c9b92d341668aedbca600fe0cad138
Federico De Faveri <federico.defaveri@fao.org>
2014-12-18 11:32:45 +0100

    Added prefixes to auto-complete

--- lib/common/components/codemirror_completion.dart
+++ lib/common/components/codemirror_completion.dart
@@ -19,12 +19,18 @@ void installHints() {
 
 }
 
-List<String> SPARQL_KEYWORDS = ["base", "prefix", "select", "distinct", "reduced", "construct", "describe",
-                             "ask", "from", "named", "where", "order", "limit", "offset", "filter", "optional",
-                             "graph", "by", "asc", "desc", "as", "having", "undef", "values", "group",
-                             "minus", "in", "not", "service", "silent", "using", "insert", "delete", "union",
-                             "true", "false", "with",
-                             "data", "copy", "to", "move", "add", "create", "drop", "clear", "load"];
+Map<String, String> PREFIXES = {
+  "rdfs": "rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
+  "rdf": "rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>",
+  "dc": "dc: <http://purl.org/dc/elements/1.1/>",
+  "geo": "geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>",
+  "owl": "owl: <http://www.w3.org/2002/07/owl#>",
+  "dct": "dct: <http://purl.org/dc/terms/>",
+  "dwc": "dwc: <http://rs.tdwg.org/dwc/terms/>",
+  "skos": "skos: <http://www.w3.org/2004/02/skos/core#>"
+};
+
+List<String> SPARQL_KEYWORDS = ["base", "prefix", "select", "distinct", "reduced", "construct", "describe", "ask", "from", "named", "where", "order", "limit", "offset", "filter", "optional", "graph", "by", "asc", "desc", "as", "having", "undef", "values", "group", "minus", "in", "not", "service", "silent", "using", "insert", "delete", "union", "true", "false", "with", "data", "copy", "to", "move", "add", "create", "drop", "clear", "load"];
 
 JsObject sparqlCompletion(JsObject jseditor, JsObject options) {
   GradeMirror editor = new GradeMirror.fromJsObject(jseditor);
@@ -32,42 +38,50 @@ JsObject sparqlCompletion(JsObject jseditor, JsObject options) {
   CodemirrorInput cmInput = editor.getOption("CodemirrorInput");
   Iterable<EditableEndpoint> endpoints = cmInput.endpoints;
   if (endpoints == null) return null;
-  
+
   Position cursorPosition = editor.getCursor();
-  
+
   String line = editor.getLine(cursorPosition.line);
   //print('line >$line<');
-  String subline = line.substring(0,cursorPosition.ch);
+  String subline = line.substring(0, cursorPosition.ch);
   //print('subline >$subline<');
-  
+
   int lastSpaceIndex = subline.lastIndexOf(new RegExp(r'\s'));
   //print('lastSpaceIndex $lastSpaceIndex');
-  
+
   String currWord = subline;
-  if (lastSpaceIndex>=0) currWord = subline.substring(lastSpaceIndex + 1, subline.length);
+  if (lastSpaceIndex >= 0) currWord = subline.substring(lastSpaceIndex + 1, subline.length);
   //print('currWord >$currWord<');
-  
+
   Set<JsObject> suggestions = new Set<JsObject>();
-  
-  if (currWord.isNotEmpty) fillMatchingKeywords(suggestions, currWord);
-  else {
-    
-    lastSpaceIndex = lastSpaceIndex>=0?lastSpaceIndex:0;
-    
+
+  if (currWord.isNotEmpty) fillMatchingKeywords(suggestions, currWord); else {
+
+    lastSpaceIndex = lastSpaceIndex >= 0 ? lastSpaceIndex : 0;
+
     subline = subline.substring(0, lastSpaceIndex);
     //print('subline >$subline<');
-    
+
     int prevSpaceIndex = subline.lastIndexOf(new RegExp(r'\s'));
     //print('prevSpaceIndex $prevSpaceIndex');
-    
-    String prevWord = subline.substring((prevSpaceIndex>=0)?prevSpaceIndex+1:0);
+
+    String prevWord = subline.substring((prevSpaceIndex >= 0) ? prevSpaceIndex + 1 : 0);
     //print('prevWord >$prevWord<');
 
-    switch(prevWord.toLowerCase()) {
-        case "graph": fillGraphs(suggestions, endpoints); break;
-        case "service": fillServices(suggestions, endpoints); break;
-        default: fillKeywords(suggestions); break;
-      }
+    switch (prevWord.toLowerCase()) {
+      case "graph":
+        fillGraphs(suggestions, endpoints);
+        break;
+      case "service":
+        fillServices(suggestions, endpoints);
+        break;
+      case "prefix":
+        fillPrefixes(suggestions);
+        break;
+      default:
+        fillKeywords(suggestions);
+        break;
+    }
   }
 
 
@@ -79,30 +93,38 @@ JsObject sparqlCompletion(JsObject jseditor, JsObject options) {
 }
 
 void fillGraphs(Set<JsObject> suggestions, Iterable<EditableEndpoint> endpoints) {
-  endpoints
-    .map((EditableEndpoint e) => e.model.graphs)
-    .expand((List<Graph> graphs) => graphs)
-    .map((Graph g)=>new JsObject.jsify({"text":"<${g.uri}>","displayText":"${g.label} - ${g.uri}"}))
-    .forEach((JsObject o) => suggestions.add(o));
+  endpoints.map((EditableEndpoint e) => e.model.graphs).expand((List<Graph> graphs) => graphs).map((Graph g) => new JsObject.jsify({
+    "text": "<${g.uri}>",
+    "displayText": "${g.label} - ${g.uri}"
+  })).forEach((JsObject o) => suggestions.add(o));
 }
 
 void fillServices(Set<JsObject> suggestions, Iterable<EditableEndpoint> endpoints) {
-  endpoints
-  .map((EditableEndpoint e) =>new JsObject.jsify({"text":"<${e.model.uri}>","displayText":e.model.name}))
-  .forEach((JsObject o) => suggestions.add(o));
+  endpoints.map((EditableEndpoint e) => new JsObject.jsify({
+    "text": "<${e.model.uri}>",
+    "displayText": e.model.name
+  })).forEach((JsObject o) => suggestions.add(o));
 }
 
 void fillMatchingKeywords(Set<JsObject> suggestions, String word) {
-  SPARQL_KEYWORDS
-  .where((String k)=>k.startsWith(word))
-  .map((String u) => new JsObject.jsify({"text":u.substring(word.length),"displayText":u}))
-  .forEach((JsObject o) => suggestions.add(o));
+  SPARQL_KEYWORDS.where((String k) => k.startsWith(word)).map((String u) => new JsObject.jsify({
+    "text": u.substring(word.length),
+    "displayText": u
+  })).forEach((JsObject o) => suggestions.add(o));
 }
 
 void fillKeywords(Set<JsObject> suggestions) {
-  SPARQL_KEYWORDS
-  .map((String u) => new JsObject.jsify({"text":u,"displayText":u}))
-  .forEach((JsObject o) => suggestions.add(o));
+  SPARQL_KEYWORDS.map((String u) => new JsObject.jsify({
+    "text": u,
+    "displayText": u
+  })).forEach((JsObject o) => suggestions.add(o));
+}
+
+void fillPrefixes(Set<JsObject> suggestions) {
+  PREFIXES.keys.map((String key) => new JsObject.jsify({
+    "text": PREFIXES[key],
+    "displayText": key
+  })).forEach((JsObject o) => suggestions.add(o));
 }
 
 
@@ -113,7 +135,7 @@ class GradeMirror extends ProxyHolder {
 
   Position getCursor([String start]) => new Position.fromProxy(start == null ? call('getCursor') : callArg('getCursor', start));
   dynamic getOption(String option) => callArg('getOption', option);
-  
+
   String getLine(int n) => callArg('getLine', n);
 
 }
