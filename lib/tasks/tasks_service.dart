c2865422077f6ad2a6aaa9b552b1501fd361abb9
Federico De Faveri <federico.defaveri@fao.org>
2014-12-19 09:51:04 +0100

    Fixed diff query check

--- lib/tasks/tasks_service.dart
+++ lib/tasks/tasks_service.dart
@@ -21,7 +21,7 @@ class TasksService extends EditableListService<Task> {
     //tmp workaround to limit the result size
     Map bean = new Map.from(task.bean);
     bean[Task.K.transform] = bean[Task.K.transform]+ " limit 1000";
-    if (bean.containsKey(Task.K.diff)) bean[Task.K.diff] = bean[Task.K.diff]+ " limit 1000";
+    if (bean[Task.K.diff] != null) bean[Task.K.diff] = bean[Task.K.diff]+ " limit 1000";
     return http.postJSon("$all_path/sandbox/$EXECUTIONS_PATH", JSON.encode(bean)).then((json) => new TaskExecution(json));
   }
 
