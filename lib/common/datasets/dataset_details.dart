02d177300423c811ed401356ae8cfd664ca454fc
Federico De Faveri <federico.defaveri@fao.org>
2015-02-19 14:36:45 +0100

    Darte formatted in dataset details

--- lib/common/datasets/dataset_details.dart
+++ lib/common/datasets/dataset_details.dart
@@ -3,9 +3,17 @@ part of datasets;
 @CustomTag("dataset-details") 
 class DatasetDetails extends PolymerElement with Filters {
   
+  List<String> dateFields = [Dataset.CREATION_DATE_FIELD]; 
+  
   @published
   Dataset item;
   
   DatasetDetails.created() : super.created();
+  
+  String getValue(String key) => isDate(key)?formatDate(item.getDate(key)):trim(item.get(key));
+  
+  bool isDate(String key) => key!=null && dateFields.contains(key);
+
+  
   
 }
--- lib/common/datasets/datasets_model.dart
+++ lib/common/datasets/datasets_model.dart
@@ -4,26 +4,23 @@ class Dataset extends GradeEntity with Filters {
 
   Dataset(Map bean) : super(bean);
   
-   static final String id = "uri";
-   static final List<String> labels = [
-                                       "label",
-                                       "http://www.w3.org/2000/01/rdf-schema#label",
-                                       "http://purl.org/dc/terms/title"
-                                      ];
-   
-  String get title {
-                    
-      for (String lbl in labels) {
-         String label = get(lbl);
-         if (label!=null)
-           return label;
-      }
+  static final String CREATION_DATE_FIELD = "http://purl.org/dc/terms/created";
 
-      return get(id);
-   }
-  
-  DateTime get creationDate => getDate("http://purl.org/dc/terms/created");
-   
+  static final String id = "uri";
+  static final List<String> labels = ["label", "http://www.w3.org/2000/01/rdf-schema#label", "http://purl.org/dc/terms/title"];
+
+  String get title {
+
+    for (String lbl in labels) {
+      String label = get(lbl);
+      if (label != null) return label;
+    }
+
+    return get(id);
+  }
+
+  DateTime get creationDate => getDate(CREATION_DATE_FIELD);
+
 }
 
 int compareDatasets(Dataset d1, Dataset d2) {
@@ -34,27 +31,26 @@ int compareDatasets(Dataset d1, Dataset d2) {
 
 
 class Datasets extends ListItems<Dataset> {
-  Datasets():super(compareDatasets);
+  Datasets() : super(compareDatasets);
 }
 
 class DatasetsPageModel extends SubPageModel<Dataset> {
-  
-  DatasetsPageModel(PageEventBus bus, ListService<Dataset> service, ListItems<Dataset> storage):super(bus, service, storage) {
-    bus.on(GraphOperation).listen((_)=>loadAll());
-    bus.on(EndpointOperation).listen((_)=>loadAll());
+
+  DatasetsPageModel(PageEventBus bus, ListService<Dataset> service, ListItems<Dataset> storage) : super(bus, service, storage) {
+    bus.on(GraphOperation).listen((_) => loadAll());
+    bus.on(EndpointOperation).listen((_) => loadAll());
   }
-  
+
   DatasetService get datasetService => service;
-  
+
   Future<bool> uploadDataset(DatasetUploadMetadata metadata, File file) {
-    return datasetService.upload(metadata, file)
-    .then((_){
+    return datasetService.upload(metadata, file).then((_) {
       loadAll();
       bus.fireInPage(new DatasetUploaded(metadata.endpointId));
       return true;
     });
   }
-  
+
 }
 
 class DatasetUploadMetadata {
@@ -63,7 +59,7 @@ class DatasetUploadMetadata {
   MediaType type;
   String endpointId;
   CSVConfiguration csvConfiguration;
-  
+
   DatasetUploadMetadata(this.name, this.author, this.type, this.endpointId, [this.csvConfiguration]);
 }
 
@@ -71,8 +67,7 @@ class CSVConfiguration {
   String delimiter;
   String encoding;
   String quote;
-  
-  CSVConfiguration(this.delimiter, this.encoding, this.quote);  
-  
-}
 
+  CSVConfiguration(this.delimiter, this.encoding, this.quote);
+
+}
