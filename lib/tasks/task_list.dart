bd35c7746817158344eef71825e586a75f3004a7
Federico De Faveri <federico.defaveri@fao.org>
2015-02-19 11:21:25 +0100

    Added invalid filter

--- lib/tasks/task_list.dart
+++ lib/tasks/task_list.dart
@@ -6,6 +6,7 @@ class TaskList extends GradeList {
   ListFilter publishFilter = new ListFilter("PUBLISH", true, (EditableTask item)=>item.model.operation == Operation.PUBLISH);
   ListFilter addFilter = new ListFilter("ADD", true, (EditableTask item)=>item.model.operation == Operation.ADD);
   ListFilter removeFilter = new ListFilter("REMOVE", true, (EditableTask item)=>item.model.operation == Operation.REMOVE);
+  ListFilter invalidFilter = new ListFilter("INVALID", false, (EditableTask item)=>!item.valid, true, true);
   ListFilter underEditAlwaysVisibleFilter = new ListFilter.hidden((EditableModel item)=>item.edit, false);
     
   KeywordFilterFunction itemFilter = (EditableModel<Task> item, String term) 
@@ -14,7 +15,7 @@ class TaskList extends GradeList {
          Filters.containsIgnoreCase(item.model.get(Task.K.diff), term);
 
   TaskList.created() : super.created('list') {
-    filters.addAll([publishFilter, addFilter, removeFilter, underEditAlwaysVisibleFilter]);
+    filters.addAll([publishFilter, addFilter, removeFilter, invalidFilter, underEditAlwaysVisibleFilter]);
     setupKeywordFilter(itemFilter);
   }
 }
