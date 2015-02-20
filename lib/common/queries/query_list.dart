201bec638123b8d88ffe4c9b3830e5654d0fecdd
Federico De Faveri <federico.defaveri@fao.org>
2015-02-20 11:18:48 +0100

    Added invalid filter to queries

--- lib/common/queries/query_list.dart
+++ lib/common/queries/query_list.dart
@@ -5,6 +5,7 @@ class QueryList extends GradeList {
   
   ListFilter servicesFilter = new ListFilter("PUBLISHED", true, (EditableQuery item)=>(item.model.status == Query.K.status_published));
   ListFilter internalFilter = new ListFilter("UNPUBLISHED", true, (EditableQuery item)=>(item.model.status == Query.K.status_unpublished && !item.model.isSystem));
+  ListFilter invalidFilter = new ListFilter("INVALID", false, (EditableQuery item)=>!item.valid && item.model.isPublished, true, true);
   ListFilter systemFilter = new ListFilter("SYSTEM", false, (EditableQuery item)=>(item.model.isSystem));
   ListFilter dataSystemFilter = new ListFilter("DATA", true, (EditableModel item)=>!item.model.isSystem);
   ListFilter underEditAlwaysVisibleFilter = new ListFilter.hidden((EditableModel item)=>item.edit, false);
@@ -24,7 +25,7 @@ class QueryList extends GradeList {
   
   void ready() {
     super.ready();
-    filters.addAll(statusEditEnabled?[servicesFilter, internalFilter, systemFilter, underEditAlwaysVisibleFilter]
+    filters.addAll(statusEditEnabled?[servicesFilter, internalFilter, invalidFilter, systemFilter, underEditAlwaysVisibleFilter]
     :[dataSystemFilter, systemFilter, underEditAlwaysVisibleFilter]);
   }
 }
