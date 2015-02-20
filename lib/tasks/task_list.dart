8a21477d66228890d5db2ba1b53616bd974ee11c
Federico De Faveri <federico.defaveri@fao.org>
2015-02-20 12:46:05 +0100

    Invalid filter for task not more exclusive

--- lib/tasks/task_list.dart
+++ lib/tasks/task_list.dart
@@ -3,10 +3,10 @@ part of tasks;
 @CustomTag("task-list")
 class TaskList extends GradeList {
   
-  ListFilter publishFilter = new ListFilter("PUBLISH", true, (EditableTask item)=>item.model.operation == Operation.PUBLISH);
-  ListFilter addFilter = new ListFilter("ADD", true, (EditableTask item)=>item.model.operation == Operation.ADD);
-  ListFilter removeFilter = new ListFilter("REMOVE", true, (EditableTask item)=>item.model.operation == Operation.REMOVE);
-  ListFilter invalidFilter = new ListFilter("INVALID", false, (EditableTask item)=>!item.valid, true, true);
+  ListFilter publishFilter = new ListFilter("PUBLISH", true, (EditableTask item)=>item.model.operation == Operation.PUBLISH && item.valid);
+  ListFilter addFilter = new ListFilter("ADD", true, (EditableTask item)=>item.model.operation == Operation.ADD && item.valid);
+  ListFilter removeFilter = new ListFilter("REMOVE", true, (EditableTask item)=>item.model.operation == Operation.REMOVE && item.valid);
+  ListFilter invalidFilter = new ListFilter("INVALID", true, (EditableTask item)=>!item.valid);
   ListFilter underEditAlwaysVisibleFilter = new ListFilter.hidden((EditableModel item)=>item.edit, false);
 
   TaskList.created() : super.created('list') {
