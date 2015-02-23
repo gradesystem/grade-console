ffa4f7d0fb722728a9a436b4078c20d430ca7468
Federico De Faveri <federico.defaveri@fao.org>
2015-02-23 13:06:50 +0100

    Fixed invalid filter

--- lib/common/queries/query_list.dart
+++ lib/common/queries/query_list.dart
@@ -5,10 +5,9 @@ class QueryList extends GradeList {
   
   ListFilter servicesFilter = new ListFilter("PUBLISHED", true, (EditableQuery item)=>(item.model.status == Query.K.status_published) && item.valid);
   ListFilter internalFilter = new ListFilter("UNPUBLISHED", true, (EditableQuery item)=>(item.model.status == Query.K.status_unpublished && !item.model.isSystem && item.valid));
-  ListFilter invalidFilter = new ListFilter("INVALID", true, (EditableQuery item)=>!item.valid && item.model.isPublished);
+  ListFilter invalidFilter = new ListFilter("INVALID", true, (EditableQuery item)=>!item.valid);
   ListFilter systemFilter = new ListFilter("SYSTEM", false, (EditableQuery item)=>(item.model.isSystem && item.valid));
   ListFilter dataSystemFilter = new ListFilter("DATA", true, (EditableModel item)=>!item.model.isSystem && item.valid);
-  ListFilter invalidStatusFilter = new ListFilter("INVALID", true, (EditableQuery item)=>!item.valid);
   ListFilter underEditAlwaysVisibleFilter = new ListFilter.hidden((EditableModel item)=>item.edit, false);
   
   @published
@@ -27,6 +26,6 @@ class QueryList extends GradeList {
   void ready() {
     super.ready();
     filters.addAll(statusEditEnabled?[servicesFilter, internalFilter, invalidFilter, systemFilter, underEditAlwaysVisibleFilter]
-    :[dataSystemFilter, invalidStatusFilter, systemFilter, underEditAlwaysVisibleFilter]);
+    :[dataSystemFilter, invalidFilter, systemFilter, underEditAlwaysVisibleFilter]);
   }
 }
