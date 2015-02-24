c9af10fd2f8b8497f68040198b6eaee92ad9d58a
Federico De Faveri <federico.defaveri@fao.org>
2015-02-24 09:41:50 +0100

    Added workaround for dropdown resizing after items modification

--- lib/common/components/grade_dropdown_menu.dart
+++ lib/common/components/grade_dropdown_menu.dart
@@ -1,5 +1,6 @@
 import 'package:polymer/polymer.dart';
 import 'package:core_elements/core_menu.dart';
+import 'package:paper_elements/paper_dropdown.dart';
 import 'package:grade_console/common.dart';
 import 'dart:html';
 
@@ -31,6 +32,7 @@ class GradeDropdownMenu extends PolymerElement {
   List<Validator> validators = [];
   
   CoreMenu menu;
+  PaperDropdown dropdown;
   
   Validator required_validator = ($) => $==null || $.isEmpty? "Please make a choice.":null;
   Validator unknown_validator;
@@ -41,11 +43,17 @@ class GradeDropdownMenu extends PolymerElement {
   ready() {
     
     menu = $["menu"] as CoreMenu;
-   unknown_validator = (sel) => (!isItem(sel))? "Current value '${sel}' is no longer a valid choice.":null;    
+    dropdown = $["dropdown"] as PaperDropdown;
+    
+    unknown_validator = (sel) => (!isItem(sel))? "Current value '${sel}' is no longer a valid choice.":null;    
    
     //observes for internal children changes (add or remove of items)
    new MutationObserver(($1,$2) {
         menu.jsElement.callMethod("updateSelected",[]);
+        
+        //forces to recalculate the dropdown size
+        dropdown.shadowRoot.querySelector("#scroller").style.height = "";
+        
         onModelChange();
       })
       .observe(this.parentNode, childList:true, subtree:true);
