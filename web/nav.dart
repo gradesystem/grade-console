de5448a9e9b44aacf926eeaf05b44dc3e8ba6c90
fabiosimeoni <fabio.simeoni@me.com>
2015-03-31 07:11:36 +0200

    first steps towards styling result navigator

--- web/nav.dart
+++ web/nav.dart
@@ -1,15 +1,13 @@
 library navigator;
 
 import 'package:polymer/polymer.dart';
-import 'package:logging/logging.dart';
+import 'dart:html';
 
-//lib-level logger
-final Logger log = new Logger('grade-navigator');
+import 'package:grade_console/common/queries/queries.dart';
+
 
 void main() {
   
-   log.fine("initialising polymer...");
-
    initPolymer().run(() {});
 }
 
@@ -18,8 +16,26 @@ void main() {
 @CustomTag("grade-nav")
 class Navigator extends PolymerElement {
   
+  @observable
+  int resultTab = 0;
+  
+  @published
+  Result result;
+  
+  @published
+  String uri;
+  
+  String endpoint;
+  
   Navigator.created() : super.created() {
     
+    Location loc = document.window.location; 
+    
+    Uri uri = Uri.parse(loc.href);
+
+    this.uri = uri.queryParameters["uri"];
+    
+    this.endpoint = uri.queryParameters["endpoint"];
   }
   
  
